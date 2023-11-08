b39260fa06a6f92ef24c4fc95e803eea0d5cd14c
jgperea <josegpermed@gmail.com>
2023-11-08 15:20:23 +0100

    Estructura principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LayoutPageComponent } from './auth/pages/layout-page/layout.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { HeroPageComponent } from './heroes/pages/hero-page/hero-page.component';
+import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
+import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
+import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutPageComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    HeroPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
