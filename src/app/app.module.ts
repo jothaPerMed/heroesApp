30ad4a3a646a687386281b480fe368802ebf30b9
jgperea <josegpermed@gmail.com>
2023-11-08 16:41:46 +0100

    Estructura AngularMaterial + PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { LayoutPageComponent } from './auth/pages/layout-page/layout.component';
-import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
-import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
-import { HeroPageComponent } from './heroes/pages/hero-page/hero-page.component';
-import { ListPageComponent } from './heroes/pages/list-page/list-page.component';
-import { NewPageComponent } from './heroes/pages/new-page/new-page.component';
-import { SearchPageComponent } from './heroes/pages/search-page/search-page.component';
 import { AuthModule } from './auth/auth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutPageComponent,
-    RegisterPageComponent,
-    LoginPageComponent,
-    HeroPageComponent,
-    ListPageComponent,
-    NewPageComponent,
-    SearchPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
     AuthModule
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { AppModule } from '../app.module';
+import { LayoutPageComponent } from './pages/layout-page/layout.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutPageComponent, LoginPageComponent, RegisterPageComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
+
   ]
 })
 export class AuthModule { }
