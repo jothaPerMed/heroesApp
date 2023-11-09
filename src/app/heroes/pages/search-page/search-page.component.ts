f8234177d042254f6ff7519c3fb87ae772a00f54
jgperea <josegpermed@gmail.com>
2023-11-09 11:12:31 +0100

    autocomplete part2

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HerosService } from '../../services/heros.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -9,7 +12,33 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchPageComponent {
 
-public searchInput = new FormControl('');
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = []
+  public selectedHeroes?: Hero;
 
+  constructor(private herosService: HerosService) {
+
+  }
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+    if (value.length === 0) return;
+    this.herosService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+
+  }
+
+
+  onSelectedOptions(event: MatAutocompleteSelectedEvent) {
+    console.log({ event });
+    if (!event.option.value) {
+      this.selectedHeroes = undefined;
+      return;
+    }
+
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+  }
 
 }
