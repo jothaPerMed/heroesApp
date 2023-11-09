fd2ec1b198c7ea27f9b87023c8ecae4ef00703dc
jgperea <josegpermed@gmail.com>
2023-11-09 11:51:42 +0100

    operaciones CRUD en el servicio

--- src/app/heroes/services/heros.service.ts
+++ src/app/heroes/services/heros.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient as http } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { enviroment } from 'src/enviroments/enviroments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -28,5 +28,25 @@ export class HerosService {
 
   getSuggestions(query:string):Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&limit=6`);
+  }
+
+
+  // Añadir
+  addHero(hero : Hero):Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  // Actualizar
+  updateHero(hero : Hero):Observable<Hero>{
+    if(!hero.id) throw Error("Hero id is required")
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id}`, hero);
+  }
+
+  // Eliminar
+  deleteHero(id : string):Observable<boolean>{
+    return this.http.delete(`${this.baseUrl}/heroes/${ id}`).pipe(
+      catchError(err=> of(false)),
+      map(resp=> true)
+    );
   }
 }
