10c4623d9bad89d5f1a1abfe88e126670c06bdaf
jgperea <josegpermed@gmail.com>
2023-11-09 13:07:30 +0100

    Añadir SnackBars

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,6 +4,7 @@ import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HerosService } from '../../services/heros.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
@@ -11,78 +12,86 @@ import { switchMap } from 'rxjs';
   styles: [
   ]
 })
-export class NewPageComponent implements OnInit{
+export class NewPageComponent implements OnInit {
 
-public heroForm= new FormGroup(
-  {
-    id: new FormControl<string>(''),
-    superhero: new FormControl<string>('', {nonNullable:true}),
-    publisher: new FormControl<Publisher>(Publisher.DCComics),
-    alter_ego: new FormControl<string>(''),
-    first_appearance: new FormControl<string>(''),
-    characters: new FormControl<string>(''),
-    alt_img: new FormControl<string>('')
+  public heroForm = new FormGroup(
+    {
+      id: new FormControl<string>(''),
+      superhero: new FormControl<string>('', { nonNullable: true }),
+      publisher: new FormControl<Publisher>(Publisher.DCComics),
+      alter_ego: new FormControl<string>(''),
+      first_appearance: new FormControl<string>(''),
+      characters: new FormControl<string>(''),
+      alt_img: new FormControl<string>('')
+    }
+  );
+  constructor(private herosService: HerosService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar) {
+
   }
-);
-constructor(private herosService: HerosService,
-            private activatedRouter: ActivatedRoute,
-            private router:Router){
-
-}
   ngOnInit(): void {
-   if(!this.router.url.includes('edit')) return;
-   this.activatedRouter.params
-   .pipe(
-    switchMap(({id})=>this.herosService.getHeroById(id)),
+    if (!this.router.url.includes('edit')) return;
+    this.activatedRouter.params
+      .pipe(
+        switchMap(({ id }) => this.herosService.getHeroById(id)),
 
-   ).subscribe(heroe=>{
-      if(!heroe) {
-        return this.router.navigateByUrl('/');
-      }
-      this.heroForm.reset(heroe);
-      return;
+      ).subscribe(heroe => {
+        if (!heroe) {
+          return this.router.navigateByUrl('/');
+        }
+        this.heroForm.reset(heroe);
+        return;
 
-   })
+      })
   }
-public publishers =[
-  {id:'DC Comics', desc:'DC - Comic'},
-  {id:'Marvel Comics', desc:'Marvel - Comic'},
-]
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comic' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comic' },
+  ]
 
 
-public onSubmit(){
+  public onSubmit() {
 
-  if(this.heroForm.invalid) return;
+    if (this.heroForm.invalid) return;
 
-  if(this.currentHero.id){
-    this.herosService.updateHero(this.currentHero).subscribe(
-      hero=>
-      //Mostrar SnakBar
-      {
-
-      });
+    if (this.currentHero.id) {
+      this.herosService.updateHero(this.currentHero).subscribe(
+        hero =>
+        //Mostrar SnakBar
+        {
+          this.showSnackBar(`${hero.superhero} updated`);
+        });
       return;
-  }else{
-    this.herosService.addHero(this.currentHero).subscribe(
-      hero=>
-      //Mostrar SnakBar y navegar a /heroes/edit/ hero.id
-      {
-
-      });
+    } else {
+      this.herosService.addHero(this.currentHero).subscribe(
+        hero =>
+        //Mostrar SnakBar y navegar a /heroes/edit/ hero.id
+        {
+          this.router.navigate(['/heroes/edit', hero.id]);
+          this.showSnackBar(`${hero.superhero} created`);
+        });
       return;
+    }
+    this.herosService.updateHero(this.currentHero);
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    });
+
   }
-  this.herosService.updateHero(this.currentHero);
-  console.log({
-    formIsValid: this.heroForm.valid,
-    value: this.heroForm.value
-  });
 
-}
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
 
-get currentHero(): Hero{
-  const hero= this.heroForm.value as Hero;
-  return hero;
-}
+  showSnackBar(message: string): void {
+    this.snackBar.open(message, 'done', {
+      duration: 2500
+    })
+  }
 
 
 }
