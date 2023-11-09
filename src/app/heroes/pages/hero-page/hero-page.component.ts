a9b6182fd618e01c27610bb9c2a3b1d555229296
jgperea <josegpermed@gmail.com>
2023-11-09 10:39:30 +0100

    buscar un heroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HerosService } from '../../services/heros.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hero } from '../../interfaces/hero.interface';
+import {delay, switchMap } from 'rxjs';
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: [
   ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+ public heroe?: Hero;
+  constructor(private heroService: HerosService, private activatedRoute : ActivatedRoute, private route:Router ){
 
+  }
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      delay(300),
+      switchMap( ({id})=>this.heroService.getHeroById(id))
+    ).subscribe(hero=>{
+      this.heroe=hero;
+      if(!this.heroe) return this.route.navigate(['/hero.list'])
+      console.log({hero});
+      return;
+
+    })
+
+  }
+
+
+
+  public goBack(){
+    this.route.navigateByUrl('heroes/list')
+  }
 }
--- src/app/heroes/services/heros.service.ts
+++ src/app/heroes/services/heros.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient as http } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { enviroment } from 'src/enviroments/enviroments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -14,5 +14,13 @@ export class HerosService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+
+  getHeroById(id:string):Observable<Hero|undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(error=>of (undefined))//se regresa un undefined en el caso que no exista
+    )
   }
 }
